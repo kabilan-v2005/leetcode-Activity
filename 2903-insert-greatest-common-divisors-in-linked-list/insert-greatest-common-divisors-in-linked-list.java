7294eaf6a18ee139218166b7dbfa8b118e607eb1
Kabilan <kabilan7305@gmail.com>
2025-10-13 19:12:54 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (21.81%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -8,28 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        LinkedList<Integer> list=new LinkedList<>();
-        ListNode temp=head;
-        while(temp!=null && temp.next!=null){
-            
-            int gcd=GCD(temp.val,temp.next.val);
-            ListNode newnode=new ListNode(gcd);
-            newnode.next=temp.next;
-            temp.next=newnode;
+        // Edge case: if list has only one node, no insertion needed
+        if (head == null || head.next == null) return head;
 
-            temp=newnode.next;
+        ListNode curr = head;
+
+        // Traverse till the second last node
+        while (curr != null && curr.next != null) {
+            int gcdVal = gcd(curr.val, curr.next.val);  // find GCD of two nodes
+            ListNode newNode = new ListNode(gcdVal);    // create a new node for GCD
+
+            // Insert newNode between curr and curr.next
+            newNode.next = curr.next;
+            curr.next = newNode;
+
+            // Move curr two steps ahead
+            curr = newNode.next;
         }
-       
+
         return head;
     }
-     public static int GCD(int a,int b){
-            while(b!=0){
-                int temp=b;
-                b=a%b;
-                a=temp;
-            }
-            return a;
+
+    // GCD helper function (Euclidean algorithm)
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
+        return a;
+    }
 }
