eab38a1619209de1ff773b005c619eb786f4e212
Kabilan <kabilan7305@gmail.com>
2025-10-18 09:42:17 +0530

    Time: 2 ms (56.94%) | Memory: 42.6 MB (90.98%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -1,11 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int maxProduct(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); // max-heap [web:3]
-        for (int x : nums) pq.offer(x); // O(n log n) total inserts [web:3]
-        int a = pq.poll(); // largest [web:3]
-        int b = pq.poll(); // second largest [web:3]
-        return (a - 1) * (b - 1); // (max1-1)*(max2-1) [web:3]
+        // PriorityQueue<Integer> q=new priorityQueue<>();
+        // for(int i:nums){
+        //     q.add(i);
+        // }
+        Arrays.sort(nums);
+        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+
     }
 }
