17d1e49f367337dc89520f3c23aba8a5a19cc297
Kabilan <kabilan7305@gmail.com>
2025-10-30 22:24:47 +0530

    Time: 3 ms (32.20%) | Memory: 45.1 MB (22.03%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] numberGame(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) pq.add(n);
+
+        int[] arr = new int[nums.length];
+        int i = 0;
+
+        while (!pq.isEmpty()) {
+            int a = pq.poll(); // smallest
+            int b = pq.poll(); // next smallest
+            arr[i++] = b;
+            arr[i++] = a;
+        }
+
+        return arr;
+    }
+}
