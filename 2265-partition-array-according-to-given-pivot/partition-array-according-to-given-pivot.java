a83cab07ea7c314c06b79994b7e25bb2b064d8bd
Kabilan <kabilan7305@gmail.com>
2026-01-07 19:27:18 +0530

    Time: 5 ms (75.18%) | Memory: 160 MB (53.38%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n=nums.length;
+        int res[] =new int[n];
+        int l=0;
+        int r=n-1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                res[l++]=nums[i];
+            }
+            if(nums[n-1-i]>pivot){
+                res[r--]=nums[n-i-1];
+            }
+        }
+        while(l<=r){
+            res[l++]=pivot;
+        }
+        return res;
+    }
+}
