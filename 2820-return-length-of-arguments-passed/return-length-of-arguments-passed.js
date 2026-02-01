e4d2a3ee8e7880aaf9dc81c9e3181461247b259c
Kabilan <kabilan7305@gmail.com>
2026-02-01 22:30:18 +0530

    Time: 41 ms (70.07%) | Memory: 53 MB (91.10%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -1,11 +1,4 @@
-/**
- * @param {...(null|boolean|number|string|Array|Object)} args
- * @return {number}
- */
 var argumentsLength = function(...args) {
     return args.length
 };
 
-/**
- * argumentsLength(1, 2, 3); // 3
- */
