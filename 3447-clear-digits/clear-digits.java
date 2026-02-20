9994993410ba1fbea4173ba9e4389aabce179a79
Kabilan <kabilan7305@gmail.com>
2026-02-20 19:06:00 +0530

    Time: 3 ms (45.92%) | Memory: 43.9 MB (67.65%) - LeetSync

--- 3447-clear-digits/clear-digits.java
+++ 3447-clear-digits/clear-digits.java
@@ -1,7 +1,6 @@
 class Solution {
     public String clearDigits(String s) {
         Stack<Character> stack=new Stack<>();
-        int c=0;
         for(char i:s.toCharArray()){
             if(Character.isDigit(i)){
                 if(!stack.isEmpty()){
@@ -10,7 +9,7 @@ class Solution {
             }
             else{
                 stack.push(i);
-                c++;
+                
             }
         }
         StringBuilder str=new StringBuilder();
