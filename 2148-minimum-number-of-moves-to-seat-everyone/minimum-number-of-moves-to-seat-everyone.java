bed1e24a19df7748955fe9ebe4b88dbe0402befb
Kabilan <kabilan7305@gmail.com>
2026-03-06 23:38:52 +0530

    Time: 5 ms (95.29%) | Memory: 46.6 MB (65.28%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -18,7 +18,7 @@ class Solution {
         // }
         // return count;
         int sum=0;
-        for(int i=0;i<students.length;i++){
+        for(int i=0;i<seats.length;i++){
             sum+=Math.abs(seats[i]-students[i]);
         }
         return sum;
