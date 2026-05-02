a7e70f2e7755a80709674eeea32ab5e56763951a
Kabilan <kabilan7305@gmail.com>
2026-05-02 15:54:06 +0530

    Time: 4 ms (59.28%) | Memory: 42.4 MB (31.90%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rotatedDigits(int n) {
+        int count=0;
+        
+        for(int i=1;i<=n;i++){
+
+            int num=i;
+            boolean isValid=true;
+            boolean isDiffer=false;
+
+            while(num>0){
+                int digit=num%10;
+
+                if(digit == 3 || digit == 4 || digit == 7){
+                    isValid = false;
+                    break;
+                }
+                if(digit == 2 || digit == 5 || digit == 6 || digit == 9){
+                    isDiffer=true;
+                }
+                num=num/10;
+            }
+                if(isValid && isDiffer){
+                    count++;
+                }
+        }
+        return count;
+    }
+}
