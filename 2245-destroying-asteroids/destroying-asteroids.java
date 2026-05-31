f60855a93c0d26a8c6c419447135fde0e56bf428
Kabilan <kabilan7305@gmail.com>
2026-05-31 19:57:25 +0530

    Time: 26 ms (81.52%) | Memory: 107.7 MB (7.85%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -2,16 +2,15 @@ class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
 
-        long curMass = mass;
-
-        for (int asteroid : asteroids) {
-            if (curMass >= asteroid) {
-                curMass += asteroid;
-            } else {
+        long curmass = mass;
+        for(int i = 0;i < asteroids.length;i++){
+            if(curmass >= asteroids[i]){
+                curmass += asteroids[i];
+            }
+            else{
                 return false;
             }
         }
-
         return true;
     }
 }
