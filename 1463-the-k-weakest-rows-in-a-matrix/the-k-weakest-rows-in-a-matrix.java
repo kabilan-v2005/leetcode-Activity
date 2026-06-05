e4b9198fb1ded844709fb21f93555f2c53918809
Kabilan <kabilan7305@gmail.com>
2026-06-05 13:53:47 +0530

    Time: 2 ms (91.83%) | Memory: 46.6 MB (47.11%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,21 +1,32 @@
 class Solution {
+    private int countSoldiers(int row[]){
+        int left = 0;
+        int rigth = row.length - 1;
+        while(left <= rigth){
+            int mid = left + (rigth - left) / 2;
+            if(row[mid] == 1) left = mid + 1;
+            else rigth = mid - 1;
+        }
+        return left;
+    }
     public int[] kWeakestRows(int[][] mat, int k) {
         int m = mat.length;
         int arr[][] = new int[m][2];
         // int flag = 0;
 
         for(int i = 0;i < m;i++){
-            int count = 0;
-            int n = mat[i].length;
-            for(int j = 0;j < n;j++){
-                if(mat[i][j] == 0){
-                    break;
-                }
-                else{
-                    count++;
-                }
-            }
-            arr[i][0] = count;
+            // int count = 0;
+            // int n = mat[i].length;
+            // for(int j = 0;j < n;j++){
+            //     // if(mat[i][j] == 0){
+            //     //     break;
+            //     // }
+            //     // else{
+            //     //     count++;
+            //     // }
+            // }
+
+            arr[i][0] = countSoldiers(mat[i]);;
             arr[i][1] = i; 
         }
         Arrays.sort(arr, (a, b) -> {
