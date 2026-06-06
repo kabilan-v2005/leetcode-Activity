2e84e868f3ed072d5527ce7c29bc16b626b95881
Kabilan <kabilan7305@gmail.com>
2026-06-06 15:49:49 +0530

    Time: 2 ms (98.84%) | Memory: 46.5 MB (68.25%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -1,26 +1,31 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
         int n = nums.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
+        // int left[] = new int[n];
+        // int right[] = new int[n];
         int leftsum = 0;
         int rightsum = 0;
-        left[0] = leftsum;
-        right[n - 1] = rightsum;
+        // left[0] = leftsum;
+        // right[n - 1] = rightsum;
 
-        for(int i = 1;i < n;i++){
-            leftsum += nums[i-1];
-            left[i] = leftsum;
-        }
+        // for(int i = 1;i < n;i++){
+        //     leftsum += nums[i-1];
+        //     left[i] = leftsum;
+        // }
 
-        for(int i = n - 2;i >= 0;i--){
-            rightsum += nums[i+1];
-            right[i] = rightsum;
-        }
+        // for(int i = n - 2;i >= 0;i--){
+        //     rightsum += nums[i+1];
+        //     right[i] = rightsum;
+        // }
         int result[] = new int[n];
+        for(int num : nums) rightsum += num;
         for(int i = 0;i < n;i++){
-            result[i] = Math.abs(left[i] - right[i]);
+            int val = nums[i];
+            rightsum -= val;
+            result[i] = Math.abs(leftsum - rightsum);
+            leftsum += val;
         }
         return result;
+        
     }
 }
