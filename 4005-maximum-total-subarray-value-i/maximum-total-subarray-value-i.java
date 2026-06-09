7faa0ca4dceed21a71b351c510c465c6ec10655b
Kabilan <kabilan7305@gmail.com>
2026-06-09 22:48:03 +0530

    Time: 1 ms (100.00%) | Memory: 62.4 MB (38.13%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -1,11 +1,13 @@
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
-        int[] sormadexin = nums;
-        int min = sormadexin[0], max = sormadexin[0];
-        for (int num : sormadexin) {
-            if (num > max) max = num;
-            if (num < min) min = num;
+        long mx = 0;
+        long mn = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num > mx) mx = num;
+            if (num < mn) mn = num;
         }
-        return (long) (max - min) * k;
+
+        return (mx - mn) * 1L * k;
     }
 }
