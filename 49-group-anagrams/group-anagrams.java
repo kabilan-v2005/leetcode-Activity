cdc1f800965604519948e59d9dd2fb341a409171
Kabilan <kabilan7305@gmail.com>
2026-07-07 23:03:33 +0530

    Time: 7 ms (73.66%) | Memory: 49.4 MB (92.26%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map = new HashMap<>();
+        HashMap<String,List<String>> map = new HashMap<>();
         
         for(int i = 0;i < strs.length;i++){
             String s = strs[i];
