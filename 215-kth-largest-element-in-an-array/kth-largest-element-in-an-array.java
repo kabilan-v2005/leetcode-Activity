1b51b26df9476771c2baa09b31f4fd7865058008
Kabilan <kabilan7305@gmail.com>
2026-07-09 01:55:33 +0530

    Time: 72 ms (23.97%) | Memory: 74.4 MB (36.23%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -4,14 +4,16 @@ class Solution {
         // Arrays.sort(nums);
         // return nums[nums.length - k ];
         int n = nums.length;
-        PriorityQueue<Integer>  pq = new PriorityQueue<>((a,b) -> b - a);
-        for(int i = 0;i < n;i++){
-            pq.offer(nums[i]);
+        int pos = k+n;
+        PriorityQueue<Integer>  pq = new PriorityQueue<>();
+        int max = 0;
+        for (int num : nums) {
+            pq.offer(num);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
-        int max = pq.poll();
-        for(int i = 1;i < k;i++){
-            max = pq.poll();
-        }
-        return max;
+        return pq.peek();
     }
 }
