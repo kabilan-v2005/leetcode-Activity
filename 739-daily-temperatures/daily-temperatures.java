0e19c7a4c34c7c65cd3d6503f9b30a3f1d2c95e2
Kabilan <kabilan7305@gmail.com>
2026-07-18 00:57:54 +0530

    Time: 76 ms (34.62%) | Memory: 106.3 MB (78.52%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -2,21 +2,21 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         Stack<Integer> stack = new Stack<>();
-        // Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
 
         int res[] = new int[n];
         // int count = 0;
         for(int i = n - 1;i >= 0; i--){
-            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]){
+            while(!stack.isEmpty() && temperatures[i] >= stack.peek()){
                 stack.pop();
                 // count--;
             }
             
-            // map.put(temperatures[i],i);
-            res[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            map.put(temperatures[i],i);
+            res[i] = stack.isEmpty() ? 0 :  map.get(stack.peek()) - i;
 
             // count++;
-            stack.push(i);
+            stack.push(temperatures[i]);
         }
 
         return res;
