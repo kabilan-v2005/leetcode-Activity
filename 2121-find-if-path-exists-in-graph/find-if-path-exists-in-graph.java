74ef1ae01a496649ede533d5e87a80700032f767
Kabilan <kabilan7305@gmail.com>
2026-07-25 10:52:36 +0530

    Time: 168 ms (20.80%) | Memory: 278.8 MB (76.43%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,6 +1,16 @@
 class Solution {
+    boolean dfs(int node,int dest,List<List<Integer>> graph,boolean visited[]){
+        if(node==dest) return true;
+        visited[node]=true;
+        for(int n:graph.get(node)){
+            if(!visited[n])
+            if(dfs(n,dest,graph,visited)) return true;
+        }
+        return false;
+    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-         List<List<Integer>> graph = new ArrayList<>();
+        
+        List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
@@ -8,24 +18,26 @@ class Solution {
             int u = edge[0];
             int v = edge[1];
             graph.get(u).add(v);
-            graph.get(v).add(u); 
+            graph.get(v).add(u);  
         }
 
-        boolean vis[] = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        vis[source] = true;
-        q.offer(source);
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            if(curr == destination) return true;
+        boolean visited[] = new boolean[n];
+        return dfs(source,destination,graph,visited);
+        // Queue<Integer> q = new LinkedList<>();
+        // vis[source] = true;
+        // q.offer(source);
+        // while(!q.isEmpty()){
+        //     int curr = q.poll();
+        //     if(curr == destination) return true;
 
-            for(int i : graph.get(curr)){
-                if(!vis[i]){
-                    vis[i] = true;
-                    q.offer(i);
-                }
-            }
-        }
-        return false;
+        //     for(int i : graph.get(curr)){
+        //         if(!vis[i]){    
+        //             vis[i] = true;
+        //             q.offer(i);
+        //         }
+        //     }
+        // }
+        // return false;
+
     }
 }
