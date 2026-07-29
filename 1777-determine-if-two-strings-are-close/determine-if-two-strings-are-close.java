9b0838df0e4fe8a23b31ed142a99b8208e2d1f91
Kabilan <kabilan7305@gmail.com>
2026-07-29 12:40:17 +0530

    Time: 10 ms (98.04%) | Memory: 47.8 MB (84.72%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
+
+        if(word1.length() != word2.length()) return false;
+        
         int freq1[] = new int[26];
         for(char ch : word1.toCharArray()){
             freq1[ch - 'a']++;
