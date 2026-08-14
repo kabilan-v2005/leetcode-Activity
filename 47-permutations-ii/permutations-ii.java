12070cfbe3e4c14eabe53f4c3870647a324ed8de
Kabilan <kabilan7305@gmail.com>
2026-08-14 23:44:28 +0530

    Time: 402 ms (5.98%) | Memory: 46.6 MB (59.83%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -13,6 +13,10 @@ class Solution {
 
         for(int i = 0;i < nums.length;i++){
             if(vis[i]) continue;
+            
+            if (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1]) {
+                continue;
+            }
 
             vis[i] = true;
             
